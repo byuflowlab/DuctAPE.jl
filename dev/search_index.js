8f5f800a4245d3d656ca32d64ae1a2a1adaeca74
Documenter.jl <documenter@juliadocs.github.io>
2022-04-21 19:46:36 +0000

    build based on 46c6b76

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DuctTAPE","category":"page"},{"location":"#DuctTAPE","page":"Home","title":"DuctTAPE","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DuctTAPE.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DuctTAPE]","category":"page"}]
+}
