8a6a1553faec771839a93a33ef4bf1f49ca1f21d
Judd Mehr <juddmehr@gmail.com>
2022-06-16 13:32:45 -0600

    try commenting out unregistered dependencies

--- src/DuctTAPE.jl
+++ src/DuctTAPE.jl
@@ -1,7 +1,7 @@
 module DuctTAPE
 
 # - DEPENDENCIES
-using FLOWFoil #panels for walls/hub
+# using FLOWFoil #panels for walls/hub
 using FLOWMath #akima splines
 using ForwardDiff #newton method
 using IterativeSolvers #SOR solver
