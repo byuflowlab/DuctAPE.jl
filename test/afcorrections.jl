68ea9c36c7720fc30cbd803425cb07add4b478e5
juddmehr <juddmehr@gmail.com>
2023-11-01 10:15:26 -0600

    start file for unit test of airfoil corrections.

---
+++ test/afcorrections.jl
@@ -0,0 +1,16 @@
+@testset "Stall Cutoffs" begin
+    # test that lift is monotonically increasing
+    @test dt.ismonotonic(cl,1)
+end
+
+@testset "Solidity and Stagger Corrections" begin
+end
+
+@testset "Prandtl-Glauert Compressibility Corrections" begin
+end
+
+@testset "Reynolds Number Corrections" begin
+end
+
+@testset "Transonic Effects Corrections" begin
+end
