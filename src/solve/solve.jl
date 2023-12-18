28ff7ba327824739d1cf3a1ef6def9f0b4a798f3
juddmehr <juddmehr@gmail.com>
2023-12-18 15:48:17 -0700

    start implementing a relaxation method for the overall solver

--- src/solve/solve.jl
+++ src/solve/solve.jl
@@ -1,3 +1,193 @@
+"""
+"""
+function solve!(Gamma, Gamma_est; maxiter=1e2, p)
+
+    # unpack parameters
+    (nrf, bt1, bt2, pf1, pf2, Vref) = p
+    maxBGamma = MVector{1,TF}(0.0)
+    maxdeltaBGamma = MVector{1,TF}(0.0)
+    maxdeltagamw = MVector{1,TF}(0.0)
+    conv = MVector{1,Bool}(false)
+    iter = MVector{1,Int}(0)
+
+    # loop until converged or max iterations are reached
+    while !conv[] && iter <= maxiter
+        # update iteration number
+        iter[] += 1
+
+        # stuff before
+
+        # in-place solve for Gamma, updating Gamma_est
+
+        # get difference between estimated Gamma and old Gamma
+        deltaG = Gamma_est .- Gamma
+
+        # relax Gamma
+        relax_Gamma!(
+            Gamma,
+            deltaG_old,
+            deltaG,
+            maxBGamma,
+            maxdeltaBGamma,
+            B;
+            nrf=nrf,
+            bt1=bt1,
+            bt2=bt2,
+            pf1=pf1,
+            pf2=pf2,
+        )
+
+        # Update rotor blade element velocities using new Gamma (and sigma?) values
+
+        # in-place solve for gamw, updated gamw_est
+
+        # get difference beetween estimated gamw and old gamw
+        deltag = gamw_est .- gamw
+
+        # relax wake gamma values
+        relax_gamw!(gamw, deltag_old, deltag, maxdeltagamw;)
+
+        # converged?
+        check_convergence!(conv, maxBGamma, maxdeltaBGamma, maxdeltagamw, Vref)
+    end
+
+    return nothing
+end
+
+"""
+- `Gamma::Array{Float}` : Array of rotor circulations (columns = rotors, rows = blade elements), updated in place
+- `delta_old_mat::Array{Float}` : Array of previous iteration's differences in circulation values, updated in place
+- `delta_mat::Array{Float}` : Array of current iteration's differences in circulation values
+- `B::Vector{Float}` : number of blades on each rotor
+- `nrf::Float=0.5` : nominal relaxation factor
+- `bt1::Float=0.2` : backtrack factor 1
+- `bt2::Float=0.6` : backtrack factor 2
+- `pf1::Float=0.4` : press forward factor 1
+- `pf2::Float=0.5` : press forward factor 2
+"""
+function relax_Gamma!(
+    Gamma,
+    delta_old_mat,
+    delta_mat,
+    maxBGamma,
+    maxdeltaBGamma,
+    B;
+    nrf=0.5,
+    bt1=0.2,
+    bt2=0.6,
+    pf1=0.4,
+    pf2=0.5,
+)
+
+    # initilize
+    TF = eltype(Gamma)
+    omega = nrf .* ones(TF, size(Gamma, 1))
+    bladeomega = MVector{1,TF}(0.5)
+
+    for (G, b, delta_old, delta) in
+        zip(eachcol(Gamma), B, eachcol(delta_old_mat), eachcol(delta_mat))
+        # - Set the normalization value based on the maximum magnitude value of B*Gamma
+
+        # find max magnitude
+        maxBGamma[], mi = findmax(abs.(G))
+
+        # maintain sign of original value
+        maxBGamma[] *= sign(G[mi])
+
+        # make sure we don't have any weird jumps
+        meang = sum(G) / length(G)
+        if meang > 0.0 # if mean is positive, make sure maxBGamma[] is at least 0.1
+            maxBGamma[] = max(maxBGamma[] * b, 0.1)
+        elseif meang < 0.0 # if mean is negative, make sure maxBGamma[] is at most -0.1
+            maxBGamma[] = min(maxBGamma[] * b, -0.1)
+        else # if the average is zero, then set maxBGamma[] to zero
+            maxBGamma[] = 0.0
+        end
+
+        # note: delta = Gamma_new .- Gamma
+        deltahat = maxBGamma[] ./ delta
+
+        # get initial relaxation factor
+        bladeomega[], oi = findmin(abs.(deltahat))
+
+        # scale relaxation factor based on if the change and old values are the same sign (back tracking or pressing forward)
+        bladeomega[] *= sign(deltahat[oi]) < 0.0 ? bt1 : pf1
+
+        # scale blade element relaxation factor
+        for (o, d, od) in zip(eachrow(omega), delta, eachrow(delta_old))
+            # if differences changed sign, use backtrack factor, if sign is same, use press forward factor
+            o[1] = bladeomega[] * (sign(d) != sign(od[1]) ? bt2 : pf2)
+
+            # save current delta into old one for next iteration
+            od[1] = d
+        end
+
+        # save max relaxation factor for convergence criteria
+        maxdeltaBGamma[] = max(maxdeltaBGamma[], omega...)
+
+        # relax Gamma for this blade
+        G .+= omega .* delta
+    end
+
+    return Gamma
+end
+
+"""
+- `gamw::Array{Float}` : Array of rotor circulations (columns = rotors, rows = blade elements), updated in place
+- `delta_old_mat::Array{Float}` : Array of previous iteration's differences in circulation values, updated in place
+- `delta_mat::Array{Float}` : Array of current iteration's differences in circulation values
+- `B::Vector{Float}` : number of blades on each rotor
+- `nrf::Float=0.5` : nominal relaxation factor
+- `bt1::Float=0.2` : backtrack factor 1
+- `bt2::Float=0.6` : backtrack factor 2
+- `pf1::Float=0.4` : press forward factor 1
+- `pf2::Float=0.5` : press forward factor 2
+"""
+function relax_gamw!(gamw, delta_old, delta, maxdeltagamw; nrf=0.5, bt=0.6, pf=1.2)
+
+    # initilize
+    TF = eltype(gamw)
+    omega = MVector{1,TF}(0.5)
+
+    # update delta gamma max for convergence criteria
+    maxdeltagamw[], mi = findmax(delta)
+    maxdeltagamw[] *= sign(delta[mi])
+
+    # use delta_old as a place holder for relaxation factor criteria
+    delta_old .*= delta
+
+    for ig in eachindex(gamw)
+
+        # choose relaxation factor based on whether old and new changes are in different or same direction
+        # note that delta_old at this point = delta_old .* delta
+        omega[] = sign(delta_old[ig]) < 0.0 ? bt * nrf : pf * nrf
+
+        # save delta_old for next iteration
+        delta_old[ig] = delta[ig] * omega[]
+
+        # update gamw value
+        gamw[ig] += delta_old[ig]
+    end
+
+    return gamw
+end
+
+"""
+"""
+function check_convergence!(
+    conv, maxBGamma, maxdeltaBGamma, maxdeltagamw, Vref; f_circ=1e-3, f_dgamw=2e-4
+)
+    conv[] =
+        abs(maxdeltaBGamma[]) < f_circ * abs(maxBGamma[]) && maxdeltagamw[] < f_dgamw * Vref
+    return conv
+end
+
+# OLD STUFF
+#
+#
+#
+#
+#
 # """
 #     analyze_propulsor(x, fx=x->x; maximum_linesearch_step_size=1e-8, iteration_limit=100)
 
