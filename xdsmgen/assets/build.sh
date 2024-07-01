19057e5b22ff0754167a7b5916c3eaa3bf145923
juddmehr <juddmehr@gmail.com>
2024-07-01 16:26:23 -0600

    clean up for a new tag

--- xdsmgen/assets/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python3 gen_xdsm.py
-
-pdflatex dtxdsm.tex
-makeindex dtxdsm.nlo -s nomencl.ist -o dtxdsm.nls
-pdflatex dtxdsm.tex
--- xdsmgen/xdsm/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 gen_xdsm.py
-
-pdflatex dtsolve.tex
