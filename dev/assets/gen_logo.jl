e88862ea5ade205bbd9d115ecaccda44f9bc2114
Documenter.jl <documenter@juliadocs.github.io>
2024-07-15 20:04:21 +0000

    build based on 8008ff9

--- dev/assets/gen_logo.jl
+++ dev/assets/gen_logo.jl
@@ -143,17 +143,18 @@ plot!(
 )
 
 # axis of rotation
-plot!([nz[1], wg[1, end, 1]], -0.001 * ones(2); color=plotsgray, label="", lw=lw - 1)
-plot!(nz[13] * ones(2), [-0.075, 0.075]; color=plotsgray, label="", lw=lw - 1)
-plot!(nz[13] * ones(2) .+ 0.075, [-0.075, 0.075]; color=plotsgray, label="", lw=lw - 1)
-plot!(wg[1, end - 5, 1] * ones(2), [-0.075, 0.075]; color=plotsgray, label="", lw=lw - 1)
-plot!(
-    wg[1, end - 5, 1] * ones(2) .- 0.075,
-    [-0.075, 0.075];
-    color=plotsgray,
-    label="",
-    lw=lw - 1,
-)
+plot!([nz[1], wg[1, end, 1]], -0.001 * ones(2); color=plotsgray, label="", lw=lw - 1, linestyle=:dashdot)
+
+# plot!(nz[13] * ones(2), [-0.075, 0.075]; color=plotsgray, label="", lw=lw - 1)
+# plot!(nz[13] * ones(2) .+ 0.075, [-0.075, 0.075]; color=plotsgray, label="", lw=lw - 1)
+# plot!(wg[1, end - 5, 1] * ones(2), [-0.075, 0.075]; color=plotsgray, label="", lw=lw - 1)
+# plot!(
+#     wg[1, end - 5, 1] * ones(2) .- 0.075,
+#     [-0.075, 0.075];
+#     color=plotsgray,
+#     label="",
+#     lw=lw - 1,
+# )
 
 ##### ----- SAVE ----- #####
 plot!(; grid=false, background_color=nothing)
