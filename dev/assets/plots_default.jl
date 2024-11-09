37a77d85943b9fcb357f64c6a3fddf856525c1b5
Documenter.jl <documenter@juliadocs.github.io>
2024-11-09 15:40:56 +0000

    build based on a5ff2ad

--- dev/assets/plots_default.jl
+++ dev/assets/plots_default.jl
@@ -2,7 +2,7 @@
 Default Plots Settings for creating tikz native figures for dissertation
 =#
 
-using Plots;
+using Plots
 gr()
 using LaTeXStrings
 using Measures
@@ -13,11 +13,6 @@ byublue = RGB(0.0, 46.0 / 255, 93.0 / 255) #BYU Navy Blue
 darkblue = RGB(0 / 255, 25 / 255, 50 / 255)
 byured = RGB(155.0 / 255, 0, 0) #"BYU" Red
 darkred = RGB(50 / 255, 0 / 255, 25 / 255)
-# middlegray = RGB(128.0 / 255, 128.0 / 255, 128.0 / 255) #Middle Gray
-# myblue = RGB(0.0 / 255, 92.0 / 255, 171.0 / 255) # royal blue
-# myred = RGB(192.0 / 255, 83.0 / 255, 103.0 / 255) # royal red
-# mygreen = RGB(143.0 / 255, 166.0 / 255, 81.0 / 255) # royal green
-# mygray = RGB(130.0 / 255, 130.0 / 255, 130.0 / 255) # royal gray
 
 primary = RGB(0, 92 / 255, 171 / 255) #blue
 secondary = RGB(190 / 255, 76 / 255, 77 / 255) #red
@@ -27,113 +22,14 @@ quinary = RGB(190 / 255, 147 / 255, 61 / 255) #yellow
 plotsgray = RGB(128 / 255, 128 / 255, 128 / 255) #gray
 
 function plots_default(;
-    #     #:Plot
-    # background_color=RGBA(1, 1, 1, 0),
     background_color=nothing,
-    # background_color_outside = nothing,
-    #     display_type,
-    dpi=300,
-    #     extra_kwargs,
-    #     extra_plot_kwargs,
-    fontfamily="Palatino_Roman",
-    # fontfamily="Palatino Roman",
+    fontfamily="cmunrm",
     foreground_color=RGB(128 / 255, 128 / 255, 128 / 255), #gray, plotsgray,
-    #     html_output_format,
-    #     inset_subplots,
-    #     layout,
-    #     link,
-    #     overwrite_figure,
-    #     plot_title,
-    #     plot_title_location,
-    #     plot_titlefontcolor,
-    #     plot_titlefontfamily,
-    #     plot_titlefonthalign,
-    #     plot_titlefontrotation,
-    #     plot_titlefontsize,
-    #     plot_titlefontvalign,
-    #     pos,
-    #     show,
-    size=(400, 300) .* 5.0 ./ 4.0, #it appears that 100 ≈ 1inch in LaTeX
-    # size=(600, 450), #it appears that 100 ≈ 1inch in LaTeX
-    # size=(800, 600), #it appears that 100 ≈ 1inch in LaTeX
-    #     tex_output_standalone,
-    #     thickness_scaling,
-    #     warn_on_unsupported,
-    #     window_title,
-
-    #######################
-    #      :Series
-    #######################
-    #     arrow,
-    #     bar_edges,
-    #     bar_position,
-    #     bar_width,
-    #     bins,
-    #     colorbar_entry,
-    #     connections,
-    #     contour_labels,
-    #     contours,
-    #     extra_kwargs,
-    #     fill_z,
-    fillalpha=0.125,
-    fillcolor=RGB(128 / 255, 128 / 255, 128 / 255),
-    #     fillrange,
-    #     group,
-    #     hover,
-    #     label,
-    #     levels,
-    #     line_z,
-    #     linealpha,
-    #     linecolor,
-    #     linestyle,
-    linewidth=1.0,
-    #     marker_z,
-    #     markeralpha,
-    #     markercolor,
-    #     markershape,
-    markersize=1,
-    markerstrokealpha=0,
-    #     markerstrokecolor,
-    #     markerstrokestyle,
-    #     markerstrokewidth,
-    #     normalize,
-    #     orientation,
-    #     primary,
-    #     quiver,
-    #     ribbon,
-    #     series_annotations,
-    #     seriesalpha,
-    #     seriescolor,
-    #     seriestype,
-    #     show_empty_bins,
-    #     smooth,
-    #     stride,
-    #     subplot,
-    #     weights,
-    #     x,
-    #     xerror,
-    #     y,
-    #     yerror,
-    #     z,
-    #     zerror
-
-    #######################
-    #      :Subplot
-    #######################
-    #     annotationcolor,
-    annotationfontfamily="Palatino_Roman",
-    annotationfontsize=10,
-    #     annotationhalign,
-    #     annotationrotation,
-    #     annotations,
-    #     annotationvalign,
-    #     aspect_ratio,
+    size=(400, 300),
+    annotationfontfamily="cmunrm",
     background_color_inside=nothing,
     background_color_legend=nothing,
     background_color_subplot=nothing,
-    #     bottom_margin,
-    #     camera,
-    #     clims,
     color_palette=[
         RGB(0, 92 / 255, 171 / 255), #blue
         RGB(190 / 255, 76 / 255, 77 / 255),
@@ -142,336 +38,29 @@ function plots_default(;
         RGB(190 / 255, 147 / 255, 61 / 255),
         RGB(128 / 255, 128 / 255, 128 / 255), #gray
     ],
-    #color_palette=[
-    #    RGB(0.0, 46.0 / 255.0, 93.0 / 255.0), #BYU Blue
-    #    RGB(155.0 / 255.0, 0.0, 0.0), #"BYU" Red
-    #    RGB(128.0 / 255.0, 128.0 / 255.0, 128.0 / 255.0), #Middle Gray
-    #    RGB(162.0 / 255.0, 227.0 / 255.0, 162.0 / 255.0), #Light Green
-    #    RGB(243.0 / 255.0, 209.0 / 255.0, 243.0 / 255.0), #Pink
-    #    RGB(205.0 / 255.0, 179.0 / 255.0, 0.0), #Yellow
-    #    RGB(161.0 / 255.0, 161.0 / 255.0, 226.0 / 255.0), #Purple
-    #],
-    #     colorbar,
-    #     colorbar_continuous_values,
-    #     colorbar_discrete_values,
-    #     colorbar_fontfamily,
-    #     colorbar_formatter,
-    #     colorbar_scale,
-    #     colorbar_tickfontcolor,
-    #     colorbar_tickfontfamily,
-    #     colorbar_tickfonthalign,
-    #     colorbar_tickfontrotation,
-    #     colorbar_tickfontsize,
-    #     colorbar_tickfontvalign,
-    #     colorbar_ticks,
-    #     colorbar_title,
-    #     colorbar_title_location,
-    #     colorbar_titlefontcolor,
-    #     colorbar_titlefontfamily,
-    #     colorbar_titlefonthalign,
-    #     colorbar_titlefontrotation,
-    #     colorbar_titlefontsize,
-    #     colorbar_titlefontvalign,
-    #     extra_kwargs,
-    #     fontfamily_subplot,
     foreground_color_legend=nothing,
-    #     foreground_color_subplot,
-    #     foreground_color_title,
-    #     framestyle = :zerolines,
-    #     left_margin,
-    # legend=false, # include legend true/false
-    #     legendfontcolor,
-    #     legendfontfamily,
-    #     legendfonthalign,
-    #     legendfontrotation,
-    #     legendfontsize,
-    #     legendfontvalign,
-    #     legendtitle,
-    #     legendtitlefontcolor,
-    #     legendtitlefontfamily,
-    #     legendtitlefonthalign,
-    #     legendtitlefontrotation,
-    #     legendtitlefontsize,
-    #     legendtitlefontvalign,
-    margin=0mm,
-    #     projection,
-    #     right_margin,
-    #     subplot_index,
-    #     title,
-    #     titlefontcolor,
-    #     titlefontfamily,
-    #     titlefonthalign,
-    #     titlefontrotation,
-    titlefontsize=14,
-    #     titlefontvalign,
-    #     titlelocation,
-    #     top_margin
-
-    #####################
-    #       :Axis
-    #####################
-    #     discrete_values,
-    #     draw_arrow,
-    #     flip,
-    #     foreground_color_axis,
-    #     foreground_color_border,
-    #     foreground_color_grid,
-    #     foreground_color_guide,
-    #     foreground_color_minor_grid,
-    #     foreground_color_text,
-    #     formatter,
+    margin=5mm,
     grid=false, # background grid true/false
-    #     gridalpha,
-    # gridlinewidth=0.5,
-    #     gridstyle,
-    #     guide,
-    #     guide_position,
-    #     guidefontcolor,
-    #     guidefontfamily,
-    #     guidefonthalign,
-    #     guidefontrotation,
-    guidefontsize=11,
-    #     guidefontvalign,
-    # ylims=(0, 3),
-    # xlims=(0, 2),
-    #     link,
-    #     minorgrid,
-    #     minorgridalpha,
-    #     minorgridlinewidth,
-    #     minorgridstyle,
-    #     minorticks,
-    #     mirror,
-    #     rotation,
-    # scale,
-    # showaxis = false, #turns off spines and tick labels, but not ticks
-    #     tick_direction,
-    #     tickfontcolor,
-    #     tickfontfamily,
-    #     tickfonthalign,
-    #     tickfontrotation,
-    tickfontsize=8,
-    #     tickfontvalign,
-    # ticks=false, #turns off tick marks
-    #     widen,
+    yguidefontrotation=-90,
 )
     Plots.default()
     Plots.default(;
-        #     #:Plot
         background_color=background_color,
-        #     display_type,
-        dpi=dpi,
-        # extra_kwargs=extra_kwargs,
-        # extra_plot_kwargs=extra_plot_kwargs,
         fontfamily=fontfamily,
         foreground_color=foreground_color,
-        #     html_output_format,
-        #     inset_subplots,
-        #     layout,
-        #     link,
-        #     overwrite_figure,
-        #     plot_title,
-        #     plot_title_location,
-        #     plot_titlefontcolor,
-        #     plot_titlefontfamily,
-        #     plot_titlefonthalign,
-        #     plot_titlefontrotation,
-        #     plot_titlefontsize,
-        #     plot_titlefontvalign,
-        #     pos,
-        #     show,
         size=size,
-        #     tex_output_standalone,
-        #     thickness_scaling,
-        #     warn_on_unsupported,
-        #     window_title,
-
-        #######################
-        #      :Series
-        #######################
-        #     arrow,
-        #     bar_edges,
-        #     bar_position,
-        #     bar_width,
-        #     bins,
-        #     colorbar_entry,
-        #     connections,
-        #     contour_labels,
-        #     contours,
-        #     extra_kwargs,
-        #     fill_z,
-        # fillalpha=fillalpha,
-        # fillcolor=fillcolor,
-        #     fillrange,
-        #     group,
-        #     hover,
-        #     label,
-        #     levels,
-        #     line_z,
-        #     linealpha,
-        #     linecolor,
-        #     linestyle,
-        linewidth=linewidth,
-        #     marker_z,
-        #     markeralpha,
-        #     markercolor,
-        #     markershape,
-        markersize=markersize,
-        markerstrokealpha=markerstrokealpha,
-        #     markerstrokecolor,
-        #     markerstrokestyle,
-        #     markerstrokewidth,
-        #     normalize,
-        #     orientation,
-        #     primary,
-        #     quiver,
-        #     ribbon,
-        #     series_annotations,
-        #     seriesalpha,
-        #     seriescolor,
-        #     seriestype,
-        #     show_empty_bins,
-        #     smooth,
-        #     stride,
-        #     subplot,
-        #     weights,
-        #     x,
-        #     xerror,
-        #     y,
-        #     yerror,
-        #     z,
-        #     zerror
-
-        #######################
-        #      :Subplot
-        #######################
-        #     annotationcolor,
         annotationfontfamily=annotationfontfamily,
-        annotationfontsize=annotationfontsize,
-        #     annotationhalign,
-        #     annotationrotation,
-        #     annotations,
-        #     annotationvalign,
-        #     aspect_ratio,
         background_color_inside=background_color_inside,
         background_color_legend=background_color_legend,
         background_color_subplot=background_color_subplot,
-        #     bottom_margin,
-        #     camera,
-        #     clims,
         color_palette=color_palette,
-        #     colorbar,
-        #     colorbar_continuous_values,
-        #     colorbar_discrete_values,
-        #     colorbar_fontfamily,
-        #     colorbar_formatter,
-        #     colorbar_scale,
-        #     colorbar_tickfontcolor,
-        #     colorbar_tickfontfamily,
-        #     colorbar_tickfonthalign,
-        #     colorbar_tickfontrotation,
-        #     colorbar_tickfontsize,
-        #     colorbar_tickfontvalign,
-        #     colorbar_ticks,
-        #     colorbar_title,
-        #     colorbar_title_location,
-        #     colorbar_titlefontcolor,
-        #     colorbar_titlefontfamily,
-        #     colorbar_titlefonthalign,
-        #     colorbar_titlefontrotation,
-        #     colorbar_titlefontsize,
-        #     colorbar_titlefontvalign,
-        #     extra_kwargs,
-        #     fontfamily_subplot,
         foreground_color_legend=foreground_color_legend,
-        #     foreground_color_subplot,
-        #     foreground_color_title,
-        #     framestyle = :zerolines,
-        #     left_margin,
-        # legend=false, # include legend true/false
-        #     legendfontcolor,
-        #     legendfontfamily,
-        #     legendfonthalign,
-        #     legendfontrotation,
-        #     legendfontsize,
-        #     legendfontvalign,
-        #     legendtitle,
-        #     legendtitlefontcolor,
-        #     legendtitlefontfamily,
-        #     legendtitlefonthalign,
-        #     legendtitlefontrotation,
-        #     legendtitlefontsize,
-        #     legendtitlefontvalign,
         margin=margin,
-        #     projection,
-        #     right_margin,
-        #     subplot_index,
-        #     title,
-        #     titlefontcolor,
-        #     titlefontfamily,
-        #     titlefonthalign,
-        #     titlefontrotation,
-        titlefontsize=titlefontsize,
-        #     titlefontvalign,
-        #     titlelocation,
-        #     top_margin
-
-        #####################
-        #       :Axis
-        #####################
-        #     discrete_values,
-        #     draw_arrow,
-        #     flip,
-        #     foreground_color_axis,
-        #     foreground_color_border,
-        #     foreground_color_grid,
-        #     foreground_color_guide,
-        #     foreground_color_minor_grid,
-        #     foreground_color_text,
-        #     formatter,
         grid=grid,
-        #     gridalpha,
-        # gridlinewidth=0.5,
-        #     gridstyle,
-        #     guide,
-        #     guide_position,
-        #     guidefontcolor,
-        #     guidefontfamily,
-        #     guidefonthalign,
-        #     guidefontrotation,
-        guidefontsize=guidefontsize,
-        #     guidefontvalign,
-        # ylims=(0, 3),
-        # xlims=(0, 2),
-        #     link,
-        #     minorgrid,
-        #     minorgridalpha,
-        #     minorgridlinewidth,
-        #     minorgridstyle,
-        #     minorticks,
-        #     mirror,
-        #     rotation,
-        # scale,
-        # showaxis = false, #turns off spines and tick labels, but not ticks
-        #     tick_direction,
-        #     tickfontcolor,
-        #     tickfontfamily,
-        #     tickfonthalign,
-        #     tickfontrotation,
-        tickfontsize=tickfontsize,
-        #     tickfontvalign,
-        # ticks=false, #turns off tick marks
-        #     widen,
+        yguidefontrotation=yguidefontrotation,
     )
 
-    return nothing
+    return (; color_palette)
 end
 
-plots_default()
-
-function latexsize(x, y)
-    return (x * 96, y * 96)
-end
-
-# - Rotate y-axis - #
-extra_kwargs = Dict(:subplot => Dict("ylabel style" => "{align=center, rotate=-90}"))
-
+custom_defaults = plots_default()
